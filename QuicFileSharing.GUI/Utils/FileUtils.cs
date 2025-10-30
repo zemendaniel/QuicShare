6022e43625f975578fe0a6c2cf5af90aeafcced9
zemendaniel <zemen.daniel_2021c@bolyai-szakkozep.hu>
2025-10-30 21:33:58 +0100

    proper path resulution

--- QuicFileSharing.GUI/Utils/FileUtils.cs
+++ QuicFileSharing.GUI/Utils/FileUtils.cs
@@ -42,21 +42,54 @@ public static class FileUtils
             return false;
         }
     }
+    // public static string? ResolveFolderPath(IStorageFolder folder)
+    // {
+    //     Console.WriteLine(folder.Path);
+    //     // Console.WriteLine(folder.Path.LocalPath);
+    //     // Console.WriteLine(folder.Path.AbsolutePath);
+    //     // Console.WriteLine(folder.Path.IsFile);
+    //     
+    //     if (folder.Path.IsAbsoluteUri)
+    //     
+    //     
+    //     var result = (Path.Exists(folder.Path.ToString()));
+    //     Console.WriteLine(folder.Path.IsFile);
+    //     Console.WriteLine(result);
+    //     return result ? folder.Path.ToString() : null;
+    //
+    //     // if (folder.Path is not { IsAbsoluteUri: true, Scheme: "file" })
+    //     //     return null;
+    //     //
+    //     // var path = Uri.UnescapeDataString(folder.Path.LocalPath);
+    //     //
+    //     // if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return path;
+    //     //
+    //     // path = path.Replace('/', '\\');
+    //     // if (folder.Path.Host != "")
+    //     //     path = $@"\\{folder.Path.Host}{path[1..]}";
+    //     //
+    //     // return path;
+    // }
     public static string? ResolveFolderPath(IStorageFolder folder)
     {
-        if (folder.Path is not { IsAbsoluteUri: true, Scheme: "file" })
+        try
+        {
+            var uri = new Uri(folder.Path.ToString(), UriKind.Absolute);
+            var path = uri.LocalPath;
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                path = path.Replace('/', '\\');
+            
+            return !Path.Exists(path) ? null : uri.LocalPath;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
             return null;
+        }
         
-        var path = Uri.UnescapeDataString(folder.Path.LocalPath);
-
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return path;
-        
-        path = path.Replace('/', '\\');
-        if (folder.Path.Host != "")
-            path = $@"\\{folder.Path.Host}{path[1..]}";
-
-        return path;
     }
+
 
 
 }
